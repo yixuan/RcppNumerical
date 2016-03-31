27605c9be044acbb9799d32c0e3d11a6793eaae2
Yixuan Qiu <yixuan.qiu@cos.name>
2016-03-31 10:09:48 -0400

    header file for package

---
+++ inst/include/RcppNumerical.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2016 Yixuan Qiu <yixuan.qiu@cos.name>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef RCPPNUMERICAL_H
+#define RCPPNUMERICAL_H
+
+#include <RcppEigen.h>
+
+// Integration
+#include "integration/GaussKronrodNodesWeights.h"
+#include "integration/Integrator.h"
+
+
+#endif // RCPPNUMERICAL_H
