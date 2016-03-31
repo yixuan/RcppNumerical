0a8e24270515dd53cd86687e636d513a35b20f48
Yixuan Qiu <yixuan.qiu@cos.name>
2016-03-31 17:58:07 -0400

    integrate R function

--- inst/include/integration/wrapper.h
+++ inst/include/integration/wrapper.h
@@ -40,6 +40,67 @@ inline double integrate(
 }
 
 
+// Integrate R function
+class RFunc: public Func
+{
+private:
+    Rcpp::Function fun;
+    Rcpp::RObject  args;
+public:
+    RFunc(Rcpp::Function fun_, Rcpp::RObject args_) :
+        fun(fun_),
+        args(args_)
+    {}
+
+    double operator()(const double& x) const
+    {
+        Rcpp::NumericVector xv = Rcpp::NumericVector::create(x);
+        Rcpp::NumericVector res = fun(xv, args);
+        if(res.length() != 1)
+            Rcpp::stop("integrand must return a vector of the same length of x");
+
+        return Rcpp::as<double>(res);
+    }
+
+    void   operator()(std::vector<double>& x) const
+    {
+        const int len = x.size();
+        Rcpp::NumericVector xv(len);
+        std::copy(x.begin(), x.end(), xv.begin());
+        Rcpp::NumericVector res = fun(xv, args);
+        if(res.length() != len)
+            Rcpp::stop("integrand must return a vector of the same length of x");
+
+        std::copy(res.begin(), res.end(), x.begin());
+    }
+};
+
+inline double integrate(
+    Rcpp::Function f, Rcpp::RObject args, const double& lower, const double& upper,
+    double& err_est, int& err_code,
+    const int subdiv = 100, const double& eps_abs = 1e-8, const double& eps_rel = 1e-6,
+    const Integrator<double>::QuadratureRule rule = Integrator<double>::GaussKronrod41
+)
+{
+    // If lower > upper, change the limits
+    double ilower = lower, iupper = upper;
+    bool change_sign = false;
+    if(lower > upper)
+    {
+        ilower = upper;
+        iupper = lower;
+        change_sign = true;
+    }
+
+    Integrator<double> intgr(subdiv);
+    RFunc rfun(f, args);
+    double res = intgr.quadratureAdaptive(rfun, ilower, iupper, eps_abs, eps_rel, rule);
+    err_est = intgr.estimatedError();
+    err_code = intgr.errorCode();
+    return change_sign ? -res : res;
+}
+
+
 }
 
 
