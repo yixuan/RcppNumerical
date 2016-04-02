b5bec885cd3ea8a5fe0d2c657469f92e151b35d1
Yixuan Qiu <yixuan.qiu@cos.name>
2016-04-02 17:55:20 -0400

    full wrapper

--- inst/include/optimization/wrapper.h
+++ inst/include/optimization/wrapper.h
@@ -7,7 +7,7 @@
 #ifndef OPTIMIZATION_WRAPPER_H
 #define OPTIMIZATION_WRAPPER_H
 
-#include <Rcpp.h>
+#include <RcppEigen.h>
 #include <R_ext/Rdynload.h>
 
 #include "lbfgs.h"
@@ -15,7 +15,7 @@
 namespace Numer
 {
 
-// Function lbfgs()
+// Function type for lbfgs()
 typedef int (*CFUN_lbfgs_TYPE)(
     int n,
     lbfgsfloatval_t *x,
@@ -25,12 +25,30 @@ typedef int (*CFUN_lbfgs_TYPE)(
     void *instance,
     lbfgs_parameter_t *param
 );
-// Function lbfgs_parameter_init()
+// Function type for lbfgs_parameter_init()
 typedef void (*CFUN_lbfgs_parameter_init_TYPE)(lbfgs_parameter_t *param);
 
 
+// Evaluation function for lbfgs()
+inline lbfgsfloatval_t lbfgs_evalfun(
+    void *instance,
+    const lbfgsfloatval_t *x,
+    lbfgsfloatval_t *g,
+    const int n,
+    const lbfgsfloatval_t step
+    )
+{
+    MFuncGrad* f = (MFuncGrad*) instance;
+    const Eigen::Map<const Eigen::VectorXd> xval(x, n);
+    Eigen::Map<Eigen::VectorXd> grad(g, n);
+
+    return f->f_grad(xval, grad);
+}
+
+
+// Main function
 inline int optim_lbfgs(
-    const MFuncGrad& f, Refvec x, double& fx_opt,
+    MFuncGrad& f, Refvec x, double& fx_opt,
     const int maxit = 300, const double& eps_f = 1e-8, const double& eps_g = 1e-6
 )
 {
@@ -40,11 +58,22 @@ inline int optim_lbfgs(
     CFUN_lbfgs_parameter_init_TYPE
         cfun_lbfgs_parameter_init = (CFUN_lbfgs_parameter_init_TYPE) R_GetCCallable("RcppNumerical", "lbfgs_parameter_init");
 
-    return 0;
+    // Prepare parameters
+    lbfgs_parameter_t param;
+    cfun_lbfgs_parameter_init(&param);
+    param.epsilon        = eps_g;
+    param.delta          = eps_f;
+    param.max_iterations = maxit;
+
+    // Call main function
+    int status = cfun_lbfgs(x.size(), x.data(), &fx_opt,
+                            lbfgs_evalfun, NULL, &f, &param);
+
+    return status;
 }
 
 
-}
+} // namespace Numer
 
 
 #endif // OPTIMIZATION_WRAPPER_H
