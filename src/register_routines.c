7bb9cd2f082e0842afbee2015d499540288ae1d1
Yixuan Qiu <yixuan.qiu@cos.name>
2016-04-02 17:03:54 -0400

    register routines

---
+++ src/register_routines.c
@@ -0,0 +1,13 @@
+#include <lbfgs.h>
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+void R_init_RcppNumerical(DllInfo *info)
+{
+    R_RegisterCCallable("RcppNumerical", "lbfgs",
+                        (DL_FUNC) lbfgs);
+    R_RegisterCCallable("RcppNumerical", "lbfgs_parameter_init",
+                        (DL_FUNC) lbfgs_parameter_init);
+}
+
