df5732e57e6d11e94bbc4d4aebacb6c32f6aacd0
Yixuan Qiu <yixuan.qiu@cos.name>
2016-04-02 20:50:09 -0400

    logistic regression using L-BFGS

---
+++ src/logistic_reg.cpp
@@ -0,0 +1,55 @@
+#include <RcppNumerical.h>
+
+using namespace Numer;
+
+typedef Eigen::Map<Eigen::MatrixXd> MapMat;
+typedef Eigen::Map<Eigen::VectorXd> MapVec;
+
+class LogisticReg: public MFuncGrad
+{
+private:
+    const MapMat X;
+    const MapVec Y;
+public:
+    LogisticReg(const MapMat x_, const MapVec y_) : X(x_), Y(y_) {}
+
+    double f_grad(Constvec& beta, Refvec grad) const
+    {
+        // Negative log likelihood
+        //   sum(log(1 + exp(X * beta))) - y' * X * beta
+
+        Eigen::VectorXd xbeta = X * beta;
+        const double yxbeta = Y.dot(xbeta);
+        // X * beta => exp(X * beta)
+        xbeta = xbeta.array().exp();
+        const double f = (xbeta.array() + 1.0).log().sum() - yxbeta;
+
+        // Gradient
+        //   X' * (p - y), p = exp(X * beta) / (1 + exp(X * beta))
+
+        // exp(X * beta) => p
+        xbeta.array() /= (xbeta.array() + 1.0);
+        grad.noalias() = X.transpose() * (xbeta - Y);
+
+        return f;
+    }
+};
+
+// [[Rcpp::export]]
+Rcpp::NumericVector logistic_reg_(Rcpp::NumericMatrix x, Rcpp::NumericVector y)
+{
+    const MapMat xx = Rcpp::as<MapMat>(x);
+    const MapVec yy = Rcpp::as<MapVec>(y);
+    // Negative log likelihood
+    LogisticReg nll(xx, yy);
+    // Initial guess
+    Eigen::VectorXd beta(xx.cols());
+    beta.fill(0.5);
+
+    double fopt;
+    int status = optim_lbfgs(nll, beta, fopt);
+    if(status != 0)
+        Rcpp::stop("fail to converge");
+
+    return Rcpp::wrap(beta);
+}
