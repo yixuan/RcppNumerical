30a5438ef1d99e30359c07d6bdb32be6aed31e13
Yixuan Qiu <yixuan.qiu@cos.name>
2016-04-06 18:06:59 -0400

    documentation

--- R/logistic_reg.R
+++ R/logistic_reg.R
@@ -1,4 +1,41 @@
-logistic_reg <- function(x, y)
+##' Fast Logistic Regression Fitting Using L-BFGS Algorithm
+##'
+##' \code{fastLR()} uses the L-BFGS algorithm to efficiently fit logistic
+##' regression. It is in fact an application of the C++ function
+##' \code{optim_lbfgs()} provided by \pkg{RcppNumerical} to perform L-BFGS
+##' optimization.
+##'
+##' @param x The model matrix
+##' @param y The response vector
+##'
+##' @return \code{fastLR()} returns a list with the following components:
+##' \item{coefficients}{Coefficient vector}
+##' \item{fitted.values}{The fitted probability values}
+##' \item{loglikelihood}{The maximized log likelihood}
+##' \item{converged}{Whether the optimization algorithm has converged}
+##'
+##' @author Yixuan Qiu \url{http://statr.me}
+##'
+##' @seealso \code{\link[stats]{glm.fit}()}
+##'
+##' @export
+##'
+##' @keywords models
+##' @keywords regression
+##' @examples
+##' set.seed(123)
+##' n = 1000
+##' p = 100
+##' x = matrix(rnorm(n * p), n)
+##' beta = runif(p)
+##' xb = c(x %*% beta)
+##' p = exp(xb) / (1 + exp(xb))
+##' y = rbinom(n, 1, p)
+##'
+##' system.time(res1 <- glm.fit(x, y, family = binomial()))
+##' system.time(res2 <- fastLR(x, y))
+##' max(abs(res1$coefficients - res2$coefficients))
+fastLR <- function(x, y)
 {
-    logistic_reg_(x, y)
+    fastLR_(x, y)
 }
