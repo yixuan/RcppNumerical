4482936f79d5aeee8a475b720bd9cb0f78637e44
Yixuan Qiu <yixuan.qiu@cos.name>
2016-04-06 20:39:44 -0400

    avoid overflow

--- src/fastLR.cpp
+++ src/fastLR.cpp
@@ -10,32 +10,40 @@ class LogisticReg: public MFuncGrad
 private:
     const MapMat X;
     const MapVec Y;
-    Eigen::VectorXd xbeta;  // contains X*beta and its transformation
+    const int n;
+    Eigen::VectorXd xbeta;  // contains X*beta
+    Eigen::VectorXd prob;   // contains log(1+exp(X*beta)) and 1/(1+exp(-X*beta))
 public:
-    LogisticReg(const MapMat x_, const MapVec y_) : X(x_), Y(y_) {}
+    LogisticReg(const MapMat x_, const MapVec y_) :
+        X(x_),
+        Y(y_),
+        n(X.rows()),
+        xbeta(n),
+        prob(n)
+    {}
 
     double f_grad(Constvec& beta, Refvec grad)
     {
         // Negative log likelihood
         //   sum(log(1 + exp(X * beta))) - y' * X * beta
-
         xbeta.noalias() = X * beta;
         const double yxbeta = Y.dot(xbeta);
-        // X * beta => exp(X * beta)
-        xbeta = xbeta.array().exp();
-        const double f = (xbeta.array() + 1.0).log().sum() - yxbeta;
+        // Calculate log(1 + exp(X * beta)), avoiding overflow
+        for(int i = 0; i < n; i++)
+            prob[i] = R::log1pexp(xbeta[i]);
+        const double f = prob.sum() - yxbeta;
 
         // Gradient
         //   X' * (p - y), p = exp(X * beta) / (1 + exp(X * beta))
-
-        // exp(X * beta) => p
-        xbeta.array() /= (xbeta.array() + 1.0);
-        grad.noalias() = X.transpose() * (xbeta - Y);
+        //                   = exp(X * beta - log(1 + exp(X * beta)))
+        prob = (xbeta - prob).array().exp();
+        grad.noalias() = X.transpose() * (prob - Y);
 
         return f;
     }
 
-    Eigen::VectorXd current_p() const { return xbeta; }
+    Eigen::VectorXd current_xb() const { return xbeta; }
+    Eigen::VectorXd current_p()  const { return prob; }
 };
 
 // [[Rcpp::export]]
@@ -46,8 +54,9 @@ Rcpp::List fastLR_(Rcpp::NumericMatrix x, Rcpp::NumericVector y)
     // Negative log likelihood
     LogisticReg nll(xx, yy);
     // Initial guess
-    Eigen::VectorXd beta(xx.cols());
-    beta.fill(0.5);
+    Rcpp::NumericVector b(xx.cols());
+    MapVec beta(b.begin(), b.length());
+    beta.setOnes();
 
     double fopt;
     int status = optim_lbfgs(nll, beta, fopt);
@@ -55,9 +64,10 @@ Rcpp::List fastLR_(Rcpp::NumericMatrix x, Rcpp::NumericVector y)
         Rcpp::warning("algorithm did not converge");
 
     return Rcpp::List::create(
-        Rcpp::Named("coefficients") = beta,
-        Rcpp::Named("fitted.values") = nll.current_p(),
-        Rcpp::Named("loglikelihood") = -fopt,
-        Rcpp::Named("converged") = (status >= 0)
+        Rcpp::Named("coefficients")      = beta,
+        Rcpp::Named("fitted.values")     = nll.current_p(),
+        Rcpp::Named("linear.predictors") = nll.current_xb(),
+        Rcpp::Named("loglikelihood")     = -fopt,
+        Rcpp::Named("converged")         = (status >= 0)
     );
 }
