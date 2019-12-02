39c2df30771add05425f8434d1abbf94aef690fb
Yixuan Qiu <yixuanq@gmail.com>
2019-12-01 21:48:09 -0500

    preserve previous API

--- inst/include/integration/wrapper.h
+++ inst/include/integration/wrapper.h
@@ -16,82 +16,90 @@
 namespace Numer
 {
 
+
+
+// Internal implementation
 namespace detail
 {
-template<class T>
+
 class transform_infinite: public Func
 {
 private:
-    T func;
-    double lower;
-    double upper;
-public:
-    transform_infinite(T _func, double _lower, double _upper) :
-    func(_func), lower(_lower), upper(_upper) {}
+    const Func& func;
+    const double lower;
+    const double upper;
+    const bool lower_finite;
+    const bool upper_finite;
 
+public:
+    transform_infinite(const Func& _func, double _lower, double _upper) :
+        func(_func), lower(_lower), upper(_upper),
+        lower_finite(lower > -std::numeric_limits<double>::infinity()),
+        upper_finite(upper < std::numeric_limits<double>::infinity())
+    {}
+
+    // Map infinite interval to (0, 1)
     double operator() (const double& t) const
     {
-        double x = (1 - t) / t;
-        bool upper_finite = (upper <  std::numeric_limits<double>::infinity());
-        bool lower_finite = (lower > -std::numeric_limits<double>::infinity());
+        const double x = (1 - t) / t;
         if (upper_finite && lower_finite)
-            Rcpp::stop("At least on limit must be infinite.");
+            Rcpp::stop("At least one limit must be infinite.");
         else if (lower_finite)
-            return func(lower + x) / pow(t, 2);
+            return func(lower + x) / (t * t);
         else if (upper_finite)
-            return func(upper - x) / pow(t, 2);
+            return func(upper - x) / (t * t);
         else
-            return (func(x) + func(-x)) / pow(t, 2);
+            return (func(x) + func(-x)) / (t * t);
     }
 };
 
-inline double integrate(
-        const Func& f, const double& lower, const double& upper,
-        double& err_est, int& err_code,
-        const int subdiv = 100, const double& eps_abs = 1e-8, const double& eps_rel = 1e-6,
-        const Integrator<double>::QuadratureRule rule = Integrator<double>::GaussKronrod41
-)
-{
-    Integrator<double> intgr(subdiv);
-    double res = intgr.quadratureAdaptive(f, lower, upper, eps_abs, eps_rel, rule);
-    err_est = intgr.estimatedError();
-    err_code = intgr.errorCode();
-    return res;
-}
 } // namespace detail
+
+
 
 //
 // [RcppNumerical API] 1-D numerical integration
 //
-template<class T>
-double integrate(const T& f, double lower, double upper,
-                 double& err_est, int& err_code,
-                 const int subdiv = 100, const double& eps_abs = 1e-8, const double& eps_rel = 1e-6,
-                 const Integrator<double>::QuadratureRule rule = Integrator<double>::GaussKronrod41
+inline double integrate(
+    const Func& f, const double& lower, const double& upper,
+    double& err_est, int& err_code,
+    const int subdiv = 100, const double& eps_abs = 1e-8, const double& eps_rel = 1e-6,
+    const Integrator<double>::QuadratureRule rule = Integrator<double>::GaussKronrod41
 )
 {
-
+    // Early exit if lower and upper limits are identical
     if (upper == lower)
     {
         err_est = 0.0;
         err_code = 0;
         return 0.0;
     }
+
+    // Finite interval
     if (std::abs(upper) < std::numeric_limits<double>::infinity() &&
         std::abs(lower) < std::numeric_limits<double>::infinity())
     {
-        return detail::integrate(f, lower, upper, err_est, err_code, subdiv, eps_abs, eps_rel, rule);
-    } else
-    {
-        double sign = 1.0;
-        if (upper < lower)
-        {
-            std::swap(upper, lower);
-            sign = -1.0;
-        }
-        detail::transform_infinite<T> g(f, lower, upper);
-        return sign * detail::integrate(g, 0.0, 1.0, err_est, err_code, subdiv, eps_abs, eps_rel, rule);
+        Integrator<double> intgr(subdiv);
+        double res = intgr.quadratureAdaptive(f, lower, upper, eps_abs, eps_rel, rule);
+        err_est = intgr.estimatedError();
+        err_code = intgr.errorCode();
+        return res;
     }
+
+    // Infinite interval
+    double sign = 1.0, lb = lower, ub = upper;
+    if (ub < lb)
+    {
+        std::swap(ub, lb);
+        sign = -1.0;
+    }
+    detail::transform_infinite g(f, lb, ub);
+
+    Integrator<double> intgr(subdiv);
+    double res = intgr.quadratureAdaptive(g, 0.0, 1.0, eps_abs, eps_rel, rule);
+    err_est = intgr.estimatedError();
+    err_code = intgr.errorCode();
+    return sign * res;
 }
 
 
