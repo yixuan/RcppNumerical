c34ff2d5e4f40dadd289f1a91841d2d789160e33
Yixuan Qiu <yixuanq@gmail.com>
2019-12-01 21:47:28 -0500

    update author information

--- inst/include/integration/wrapper.h
+++ inst/include/integration/wrapper.h
@@ -1,4 +1,5 @@
-// Copyright (C) 2016 Yixuan Qiu <yixuan.qiu@cos.name>
+// Copyright (C) 2016-2019 Yixuan Qiu <yixuan.qiu@cos.name>
+// Copyright (C) 2019 Ralf Stubner <ralf.stubner@gmail.com>
 //
 // This Source Code Form is subject to the terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
